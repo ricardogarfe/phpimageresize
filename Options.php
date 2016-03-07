31958f1e15cdaa3b1a622bbc09863fd8c39b69dd
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-03-07 19:31:36 +0100

    Assert asHash with default options

--- Options.php
+++ Options.php
@@ -1,6 +1,23 @@
 <?php
 
-class Options
-{
+class Options {
 
+    private $opts;
+
+    public function __construct($opts=array())
+    {
+        $cacheFolder = './cache/';
+        $remoteFolder = $cacheFolder . 'remote/';
+
+        $defaults = array('crop' => false, 'scale' => 'false', 'thumbnail' => false, 'maxOnly' => false,
+            'canvas-color' => 'transparent', 'output-filename' => false,
+            'cacheFolder' => $cacheFolder, 'remoteFolder' => $remoteFolder, 'quality' => 90, 'cache_http_minutes' => 20);
+
+        $this->opts = array_merge($defaults, $opts);
+    }
+
+    public function asHash()
+    {
+        return $this->opts;
+    }
 }
--- test/FunctionResizeTest.php
+++ test/FunctionResizeTest.php
@@ -3,8 +3,29 @@
 include '../Options.php';
 
 class FunctionResizeTest extends PHPUnit_Framework_TestCase {
+
     public function testOpts() {
         $this->assertInstanceOf('Options', new Options);
+    }
+
+    public function testDefaults() {
+        $options = new Options();
+        $defaults = array(
+            'crop' => false,
+            'scale' => 'false',
+            'thumbnail' => false,
+            'maxOnly' => false,
+            'canvas-color' => 'transparent',
+            'output-filename' => false,
+            'cacheFolder' => './cache/',
+            'remoteFolder' => './cache/remote/',
+            'quality' => 90,
+            'cache_http_minutes' => 20
+        );
+
+        $asHash = $options->asHash();
+
+        $this->assertEquals($defaults, $asHash);
     }
 }
 
