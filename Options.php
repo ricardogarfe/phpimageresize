b0fa8357f080680a2aba3d9a0003423bc977848e
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-03-10 17:47:39 +0100

    Create cache and remote folder constants.

--- Options.php
+++ Options.php
@@ -1,17 +1,16 @@
 <?php
 
 class Options {
+    const CACHE_FOLDER = './cache/';
+    const REMOTE_FOLDER = './cache/remote/';
 
     private $opts;
 
-    public function __construct($opts=array())
-    {
-        $cacheFolder = './cache/';
-        $remoteFolder = $cacheFolder . 'remote/';
+    public function __construct($opts=array()) {
 
         $defaults = array('crop' => false, 'scale' => 'false', 'thumbnail' => false, 'maxOnly' => false,
             'canvas-color' => 'transparent', 'output-filename' => false,
-            'cacheFolder' => $cacheFolder, 'remoteFolder' => $remoteFolder, 'quality' => 90, 'cache_http_minutes' => 20);
+            'cacheFolder' => self::CACHE_FOLDER, 'remoteFolder' => self::REMOTE_FOLDER, 'quality' => 90, 'cache_http_minutes' => 20);
 
         $this->opts = array_merge($defaults, $opts);
     }
