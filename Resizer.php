35c1246aac005462344ece9669954f520eba1f70
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-03-14 20:19:50 +0100

    Create Resizer object

---
+++ Resizer.php
@@ -0,0 +1,17 @@
+<?php
+
+class Resizer {
+
+    public function __construct($path, $configuration=null) {
+        $this->checkPath($path);
+        $this->checkConfiguration($configuration);
+    }
+
+    private function checkPath($path) {
+        if(!($path instanceof ImagePath))throw new InvalidArgumentException();
+    }
+
+    private function checkConfiguration($configuration) {
+        if (!($configuration instanceof Configuration)) throw new InvalidArgumentException();
+    }
+}
---
+++ test/ResizerTest.php
@@ -0,0 +1,30 @@
+<?php
+
+require '../Resizer.php';
+require '../ImagePath.php';
+require '../Configuration.php';
+
+class ResizerTest extends PHPUnit_Framework_TestCase {
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testNecessaryCollaboration() {
+        $resizer = new Resizer('anyNonPathObject');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testOptionalCollaboration() {
+        $resizer = new Resizer(new ImagePath(''), 'nonConfigurationObject');
+    }
+
+    /**
+     * @expectedException InvalidArgumentException
+     */
+    public function testInstantiation() {
+        $this->assertInstanceOf('Resizer', new Resizer(new ImagePath(''), new Configuration()));
+        $this->assertInstanceOf('Resizer', new Resizer(new ImagePath('')));
+    }
+}
