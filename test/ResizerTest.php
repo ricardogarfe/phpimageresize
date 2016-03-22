aea1b86e4f0baea2b0b939f9e80fbe845c653310
Ricardo García Fernández <ricardogarfe@gmail.com>
2016-03-22 10:44:12 +0100

    Prepare test to fail finding a path

--- test/ResizerTest.php
+++ test/ResizerTest.php
@@ -3,6 +3,7 @@
 require '../Resizer.php';
 require '../ImagePath.php';
 require '../Configuration.php';
+date_default_timezone_set('Europe/Berlin');
 
 class ResizerTest extends PHPUnit_Framework_TestCase {
 
@@ -30,9 +31,24 @@ class ResizerTest extends PHPUnit_Framework_TestCase {
 
         $stub = $this->getMockBuilder('FileSystem')
             ->getMock();
-
         $stub->method('file_get_contents')
             ->willReturn('foo');
+
+        $resizer->injectFileSystem($stub);
+
+        $this->assertEquals('./cache/remote/Disappointed-Meme-Face-08.png', $resizer->obtainFilePath());
+
+    }
+
+    public function testLocallyCachedFilePathFail() {
+        $resizer = new Resizer(new ImagePath('http://memesvault.com/wp-content/uploads/Disappointed-Meme-Face-08.png?q=alt'));
+
+        $stub = $this->getMockBuilder('FileSystem')
+            ->getMock();
+        $stub->method('file_exists')
+            ->willReturn(true);
+        $stub->method('filemtime')
+            ->willReturn(21 * 60);
 
         $resizer->injectFileSystem($stub);
 
